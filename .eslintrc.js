b721564282f8bd50232e4fa2a60fe4f45463419a
Zamba Izo <ofuzak@gmail.com>
2022-05-06 17:03:14 +0300

    disable prettier config for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
-    "plugin:prettier/recommended",
+    //"plugin:prettier/recommended",
   ],
 
   /* parserOptions: {
